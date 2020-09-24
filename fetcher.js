94128b32ef9f03d3b8da9b162a762a181b4397a4
helenohyeah <ouyang.helen@gmail.com>
2020-09-24 02:31:02 +0000

    Completed path already exists edge case

--- fetcher.js
+++ fetcher.js
@@ -1,76 +1,46 @@
 const request = require('request');
-const fs = require ('fs');
-// const stdin = process.stdin;
-// const stdout = process.stdout;
-// stdin.setRawMode(true);
-// stdin.setEncoding('utf8');
+const fs = require('fs');
+const readline = require('readline');
 
-// const rl = readline.createInterface({
-//   input: stdin,
-//   output: stdout
-// });
+const stdin = process.stdin;
+const stdout = process.stdout;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+const rl = readline.createInterface({
+  input: stdin,
+  output: stdout
+});
 
 const userInput = process.argv.slice(2);
 const url = userInput[0];
 const localPath = userInput[1];
 
-// const askUser = (localPath, body) => {
-//   rl.question('File already exists. Overwrite? (Y/N)')
-  
-//   stdin.resume();
-//   stdout.write(`The file already exists. Overwrite? (Y/N): `);
-//   stdin.on('data', (data) => {
-//     if (data === 'y') {
-//       // overwrite file
-//       fs.writeFile(localPath, body, () => {
-//       console.log('Overwrite successful');
-//       });
-//     }
-//     if (data === 'n') process.exit();
-//   })
-// };
-
+const askUser = (question, localPath, body) => {
+  rl.question(question, (answer) => {
+    if (answer.toLowerCase() === 'y') {
+      fs.writeFile(localPath, body, () => {
+        console.log('File contents overwritten');
+        process.exit();
+      });
+    } else if (answer.toLowerCase() === 'n') {
+      process.exit();
+    } else {
+      askUser('Invalid entry. Overwrite? (Y/N): ', localPath, body);
+    }
+  });
+};
 
 request(url, (error, response, body) => {
-
+  // check if file exists
   fs.access(localPath, (err) => {
-    if (err) {
+    if (err) { // file doesn't exist case
       fs.writeFile(localPath, body, () => {
-      console.log('File contents successfully copied');
+        console.log('File contents successfully copied');
+        process.exit();
       });
-    } else {
-      fs.writeFile(localPath, body, () => {
-      console.log('File contents overwritten');
-      });
+    } else { // file exists case
+      askUser('File already exists. Overwrite? (Y/N): ', localPath, body);
     }
-    // } else {
-    //   askUser(localPath, body);
-    // }
   });
 });
-
-
-//   // writes contents to file
-//   fs.writeFile(path, content, () => {
-//   console.log('File contents successfully copied');
-//   });
-// });
-
-
-// request(url, (error, response, body) => {
-//   fs.writeFile(localPath, body, () => {
-//     if (localPath) {
-//       askUser();
-//       if (askUser() === false) process.exit();
-//     }
-//     console.log(`File contents copied into ${localPath}`);
-//   })
-// })
-
-// fs.readFile(localPath, 'utf8', (err, data) => {
-//   // console.log(data);
-//   print(data);
-// });
-
-// const print = (data) => console.log(data);
-// console.log('url', url, ', local path', localPath);
